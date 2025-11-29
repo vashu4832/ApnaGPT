737373dea2b427479e0a64fafd7a2cebd473854a
ashu <ashutoshvishwakarma222@gmail.com>
2025-11-29 20:35:49 +0530

    chat-message component

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,10 +1,31 @@
 import './ChatWindow.css';
+import Chat from './Chat.jsx';
 
 function ChatWindow(){
     return(
         <>
-            <div>
-                ChatWindow
+            <div className='chatWindow'>
+                <div className='navbar'>
+                    <span>ApnaGPT<i class="fa-solid fa-angle-down"></i></span>
+                    <div className="userIconDiv">
+                        <span className='userIcon'><i class="fa-solid fa-user"></i></span>
+                    </div>
+                </div>
+                <Chat />
+                <div className="chatInput">
+                    <div className="inputBox">
+                        <input placeholder='Ask anything'>
+
+                        </input>
+                        <div id='submit'>
+                            <i class="fa-solid fa-paper-plane"></i>
+                        </div>
+                    </div>
+                    <div className="info">
+                        ApnaGPT can makes mistakes, check important info. See Cookie Preferences.
+                    </div>
+                </div>
+
             </div>
         </>
     )
