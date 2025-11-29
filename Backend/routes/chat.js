7e9d74a76d5e5c8d28707c46b59cfa88d955a3ef
ashu <ashutoshvishwakarma222@gmail.com>
2025-11-29 16:16:08 +0530

    completed backend

---
+++ Backend/routes/chat.js
@@ -0,0 +1,103 @@
+import express, { Router } from "express";
+import Thread from "../models/thread.js";
+import thread from "../models/thread.js";
+import getOpenAIAPIResponse from "../utils/openai.js";
+
+const router = express.Router();
+
+// test
+router.post("/test", async (req, res) => {
+    try {
+        const thread = new Thread({
+            threadId: "vashu",
+            title: "Testing New Thread"
+        });
+        const response = await thread.save();
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Failed to save in DB" });
+    }
+});
+
+// Get all threads
+router.get("/thread", async (req, res) => {
+    try {
+        const threads = await Thread.find({});
+        // descending order of updatedAt... most recent data on top
+        res.json(threads);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Failed to fetch threads" });
+    }
+});
+
+router.get("/thread/:threadId", async (req, res) => {
+    const { threadId } = req.params;
+
+    try {
+        await Thread.findOne({ threadId });
+
+        if (!thread) {
+            res.status(404).json({ error: "Thread not found" });
+        }
+
+        res.json(thread.messages);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Failed to fetch chat" });
+    }
+});
+
+router.delete("/thread/:threadId", async (req, res) => {
+    const { threadId } = req.params;
+
+    try {
+        const deletedThread = await Thread.findOneAndDelete({ threadId });
+
+        if (!deletedThread) {
+            res.status(404).json({ error: "Thread not found" });
+        }
+
+        res.status(200).json({ success: "Thread deleted sucessfully" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Failed to delete thread" });
+    }
+});
+
+router.post("/chat", async (req, res) => {
+    const { threadId, message } = req.body;
+
+    if (!threadId || !message) {
+        res.status(400).json({ error: "missing required fields" });
+    }
+
+    try {
+
+        const thread = await Thread.findOne({threadId});
+
+        if(!thread){
+            // create a new thread
+            thread = new Thread({
+                threadId,
+                title: message,
+                messages: [{role: "user", content: message}]
+            });
+        } else {
+            thread.messages.push({role: "user", content: message})
+        }
+
+        const assistantReply = await getOpenAIAPIResponse(message);
+
+        thread.messages.push({role: "assistant", content: assistantReply});
+        thread.updatedAt = new Date();
+
+        await thread.save(); 
+        res.json({reply: assistantReply});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "something went wrong" });
+    }
+})
+
+export default router;
---
+++ Backend/server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import 'dotenv/config';
+import cors from "cors";
+import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", chatRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+    connectDB();
+});
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("connected with database.")
+    } catch(err) {
+        console.log("Failed to connect with DB", err);
+    }
+}
+
+// app.post("/test", async(req, res) => {
+//     const options = {
+//         method: "POST",
+//         headers : {
+//             "Content-Type": "application/json",
+//             "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+//         },
+//         body: JSON.stringify({
+//             "model": "gpt-4o-mini",
+//             messages: [{
+//                 role: "user",
+//                 content: req.body.message
+//             }]
+//         })
+//     }
+
+//     try{
+//         const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+//         const data = await response.json();
+//         // console.log(data.choices[0].message.content);
+//         res.send(data.choices[0].message.content);
+//     } catch(err) {
+//         console.log(err);
+//     }
+// })
+
