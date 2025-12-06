4e2137242778b2a5d1e2829e8ac314f2b167e66d
ashu <ashutoshvishwakarma222@gmail.com>
2025-12-06 10:28:43 +0530

    fix import error

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import Thread from "../models/thread.js";
+import Thread from "../models/Thread.js";
 import getOpenAIAPIResponse from "../utils/openai.js";
 
 const router = express.Router();
